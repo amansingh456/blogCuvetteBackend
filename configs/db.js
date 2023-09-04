aa7babbe6b1a67796625232afc0a23166a0f761c
amansingh456 <amansinghrajawat690@gmail.com>
2023-09-04 10:35:04 +0530

    .

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 // require("dotenv").config()
 
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "localhost:3600",
   user: "root",
   password: "aman",
   database: "blogcuvette",
