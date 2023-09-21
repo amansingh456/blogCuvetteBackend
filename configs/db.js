cda771c452e5fc94a0b516f3e642244697be5b00
Aman Singh Rajawat <110046883+amansingh456@users.noreply.github.com>
2023-09-21 19:38:17 +0530

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 // require("dotenv").config()
 
 const db = mysql.createConnection({
-  host: "sql12.freesqldatabase.com",
-  user: "sql12646424",
-  password: "X6iTjmu4F8",
-  database: "sql12646424",
+  host: "db4free.net",
+  user: "cuvette12blog",
+  password: "cuvette12blog",
+  database: "cuvette12blog",
 });
 
 module.exports = db;
